fa7eca5bab116be5ddc4310caa090b8ab83feddc
BeBack <BeBack@192.168.1.41>
2013-02-05 22:05:37 +0100

    Initial commit

---
+++ src/generated/StateMachineBase.java
@@ -0,0 +1,467 @@
+/**
+ * This class contains generated code from the Codename One Designer, DO NOT MODIFY!
+ * This class is designed for subclassing that way the code generator can overwrite it
+ * anytime without erasing your changes which should exist in a subclass!
+ * For details about this file and how it works please read this blog post:
+ * http://codenameone.blogspot.com/2010/10/ui-builder-class-how-to-actually-use.html
+*/
+package generated;
+
+import com.codename1.ui.*;
+import com.codename1.ui.util.*;
+import com.codename1.ui.plaf.*;
+import com.codename1.ui.events.*;
+
+public abstract class StateMachineBase extends UIBuilder {
+    private Container aboutToShowThisContainer;
+    /**
+     * this method should be used to initialize variables instead of
+     * the constructor/class scope to avoid race conditions
+     */
+    /**
+    * @deprecated use the version that accepts a resource as an argument instead
+    
+**/
+    protected void initVars() {}
+
+    protected void initVars(Resources res) {}
+
+    public StateMachineBase(Resources res, String resPath, boolean loadTheme) {
+        startApp(res, resPath, loadTheme);
+    }
+
+    public Container startApp(Resources res, String resPath, boolean loadTheme) {
+        initVars();
+        UIBuilder.registerCustomComponent("Form", com.codename1.ui.Form.class);
+        UIBuilder.registerCustomComponent("InfiniteProgress", com.codename1.components.InfiniteProgress.class);
+        UIBuilder.registerCustomComponent("MultiList", com.codename1.ui.list.MultiList.class);
+        UIBuilder.registerCustomComponent("Label", com.codename1.ui.Label.class);
+        UIBuilder.registerCustomComponent("Container", com.codename1.ui.Container.class);
+        if(loadTheme) {
+            if(res == null) {
+                try {
+                    if(resPath.endsWith(".res")) {
+                        res = Resources.open(resPath);
+                        System.out.println("Warning: you should construct the state machine without the .res extension to allow theme overlays");
+                    } else {
+                        res = Resources.openLayered(resPath);
+                    }
+                } catch(java.io.IOException err) { err.printStackTrace(); }
+            }
+            initTheme(res);
+        }
+        if(res != null) {
+            setResourceFilePath(resPath);
+            setResourceFile(res);
+            initVars(res);
+            return showForm("Main", null);
+        } else {
+            Form f = (Form)createContainer(resPath, "Main");
+            initVars(fetchResourceFile());
+            beforeShow(f);
+            f.show();
+            postShow(f);
+            return f;
+        }
+    }
+
+    public Container createWidget(Resources res, String resPath, boolean loadTheme) {
+        initVars();
+        UIBuilder.registerCustomComponent("Form", com.codename1.ui.Form.class);
+        UIBuilder.registerCustomComponent("InfiniteProgress", com.codename1.components.InfiniteProgress.class);
+        UIBuilder.registerCustomComponent("MultiList", com.codename1.ui.list.MultiList.class);
+        UIBuilder.registerCustomComponent("Label", com.codename1.ui.Label.class);
+        UIBuilder.registerCustomComponent("Container", com.codename1.ui.Container.class);
+        if(loadTheme) {
+            if(res == null) {
+                try {
+                    res = Resources.openLayered(resPath);
+                } catch(java.io.IOException err) { err.printStackTrace(); }
+            }
+            initTheme(res);
+        }
+        return createContainer(resPath, "Main");
+    }
+
+    protected void initTheme(Resources res) {
+            String[] themes = res.getThemeResourceNames();
+            if(themes != null && themes.length > 0) {
+                UIManager.getInstance().setThemeProps(res.getTheme(themes[0]));
+            }
+    }
+
+    public StateMachineBase() {
+    }
+
+    public StateMachineBase(String resPath) {
+        this(null, resPath, true);
+    }
+
+    public StateMachineBase(Resources res) {
+        this(res, null, true);
+    }
+
+    public StateMachineBase(String resPath, boolean loadTheme) {
+        this(null, resPath, loadTheme);
+    }
+
+    public StateMachineBase(Resources res, boolean loadTheme) {
+        this(res, null, loadTheme);
+    }
+
+    public com.codename1.ui.Container findContainer3(Component root) {
+        return (com.codename1.ui.Container)findByName("Container3", root);
+    }
+
+    public com.codename1.ui.Container findContainer3() {
+        com.codename1.ui.Container cmp = (com.codename1.ui.Container)findByName("Container3", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Container)findByName("Container3", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Container findContainer2(Component root) {
+        return (com.codename1.ui.Container)findByName("Container2", root);
+    }
+
+    public com.codename1.ui.Container findContainer2() {
+        com.codename1.ui.Container cmp = (com.codename1.ui.Container)findByName("Container2", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Container)findByName("Container2", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Container findContainer1(Component root) {
+        return (com.codename1.ui.Container)findByName("Container1", root);
+    }
+
+    public com.codename1.ui.Container findContainer1() {
+        com.codename1.ui.Container cmp = (com.codename1.ui.Container)findByName("Container1", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Container)findByName("Container1", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Form findULPSpashScreen(Component root) {
+        return (com.codename1.ui.Form)findByName("ULP_SpashScreen", root);
+    }
+
+    public com.codename1.ui.Form findULPSpashScreen() {
+        com.codename1.ui.Form cmp = (com.codename1.ui.Form)findByName("ULP_SpashScreen", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Form)findByName("ULP_SpashScreen", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.components.InfiniteProgress findInfiniteProgress(Component root) {
+        return (com.codename1.components.InfiniteProgress)findByName("InfiniteProgress", root);
+    }
+
+    public com.codename1.components.InfiniteProgress findInfiniteProgress() {
+        com.codename1.components.InfiniteProgress cmp = (com.codename1.components.InfiniteProgress)findByName("InfiniteProgress", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.components.InfiniteProgress)findByName("InfiniteProgress", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.list.MultiList findMultiList1(Component root) {
+        return (com.codename1.ui.list.MultiList)findByName("MultiList1", root);
+    }
+
+    public com.codename1.ui.list.MultiList findMultiList1() {
+        com.codename1.ui.list.MultiList cmp = (com.codename1.ui.list.MultiList)findByName("MultiList1", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.list.MultiList)findByName("MultiList1", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Form findMain(Component root) {
+        return (com.codename1.ui.Form)findByName("Main", root);
+    }
+
+    public com.codename1.ui.Form findMain() {
+        com.codename1.ui.Form cmp = (com.codename1.ui.Form)findByName("Main", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Form)findByName("Main", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Label findLabel1(Component root) {
+        return (com.codename1.ui.Label)findByName("Label1", root);
+    }
+
+    public com.codename1.ui.Label findLabel1() {
+        com.codename1.ui.Label cmp = (com.codename1.ui.Label)findByName("Label1", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Label)findByName("Label1", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Label findLabel2(Component root) {
+        return (com.codename1.ui.Label)findByName("Label2", root);
+    }
+
+    public com.codename1.ui.Label findLabel2() {
+        com.codename1.ui.Label cmp = (com.codename1.ui.Label)findByName("Label2", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Label)findByName("Label2", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Label findLabel(Component root) {
+        return (com.codename1.ui.Label)findByName("Label", root);
+    }
+
+    public com.codename1.ui.Label findLabel() {
+        com.codename1.ui.Label cmp = (com.codename1.ui.Label)findByName("Label", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Label)findByName("Label", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.list.MultiList findMultiList(Component root) {
+        return (com.codename1.ui.list.MultiList)findByName("MultiList", root);
+    }
+
+    public com.codename1.ui.list.MultiList findMultiList() {
+        com.codename1.ui.list.MultiList cmp = (com.codename1.ui.list.MultiList)findByName("MultiList", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.list.MultiList)findByName("MultiList", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.list.MultiList findMultiList11(Component root) {
+        return (com.codename1.ui.list.MultiList)findByName("MultiList11", root);
+    }
+
+    public com.codename1.ui.list.MultiList findMultiList11() {
+        com.codename1.ui.list.MultiList cmp = (com.codename1.ui.list.MultiList)findByName("MultiList11", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.list.MultiList)findByName("MultiList11", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Container findContainer(Component root) {
+        return (com.codename1.ui.Container)findByName("Container", root);
+    }
+
+    public com.codename1.ui.Container findContainer() {
+        com.codename1.ui.Container cmp = (com.codename1.ui.Container)findByName("Container", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Container)findByName("Container", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    protected void exitForm(Form f) {
+        if("ULP_SpashScreen".equals(f.getName())) {
+            exitULPSpashScreen(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(f.getName())) {
+            exitMain(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+    }
+
+
+    protected void exitULPSpashScreen(Form f) {
+    }
+
+
+    protected void exitMain(Form f) {
+    }
+
+    protected void beforeShow(Form f) {
+    aboutToShowThisContainer = f;
+        if("ULP_SpashScreen".equals(f.getName())) {
+            beforeULPSpashScreen(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(f.getName())) {
+            beforeMain(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+    }
+
+
+    protected void beforeULPSpashScreen(Form f) {
+    }
+
+
+    protected void beforeMain(Form f) {
+    }
+
+    protected void beforeShowContainer(Container c) {
+    aboutToShowThisContainer = c;
+        if("ULP_SpashScreen".equals(c.getName())) {
+            beforeContainerULPSpashScreen(c);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(c.getName())) {
+            beforeContainerMain(c);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+    }
+
+
+    protected void beforeContainerULPSpashScreen(Container c) {
+    }
+
+
+    protected void beforeContainerMain(Container c) {
+    }
+
+    protected void postShow(Form f) {
+        if("ULP_SpashScreen".equals(f.getName())) {
+            postULPSpashScreen(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(f.getName())) {
+            postMain(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+    }
+
+
+    protected void postULPSpashScreen(Form f) {
+    }
+
+
+    protected void postMain(Form f) {
+    }
+
+    protected void postShowContainer(Container c) {
+        if("ULP_SpashScreen".equals(c.getName())) {
+            postContainerULPSpashScreen(c);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(c.getName())) {
+            postContainerMain(c);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+    }
+
+
+    protected void postContainerULPSpashScreen(Container c) {
+    }
+
+
+    protected void postContainerMain(Container c) {
+    }
+
+    protected void onCreateRoot(String rootName) {
+        if("ULP_SpashScreen".equals(rootName)) {
+            onCreateULPSpashScreen();
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(rootName)) {
+            onCreateMain();
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+    }
+
+
+    protected void onCreateULPSpashScreen() {
+    }
+
+
+    protected void onCreateMain() {
+    }
+
+    protected boolean setListModel(List cmp) {
+        String listName = cmp.getName();
+        if("MultiList1".equals(listName)) {
+            return initListModelMultiList1(cmp);
+        }
+        if("MultiList".equals(listName)) {
+            return initListModelMultiList(cmp);
+        }
+        if("MultiList11".equals(listName)) {
+            return initListModelMultiList11(cmp);
+        }
+        return super.setListModel(cmp);
+    }
+
+    protected boolean initListModelMultiList1(List cmp) {
+        return false;
+    }
+
+    protected boolean initListModelMultiList(List cmp) {
+        return false;
+    }
+
+    protected boolean initListModelMultiList11(List cmp) {
+        return false;
+    }
+
+    protected void handleComponentAction(Component c, ActionEvent event) {
+        Container rootContainerAncestor = getRootAncestor(c);
+        if(rootContainerAncestor == null) return;
+        String rootContainerName = rootContainerAncestor.getName();
+        if(c.getParent().getLeadParent() != null) {
+            c = c.getParent().getLeadParent();
+        }
+        if(rootContainerName == null) return;
+        if(rootContainerName.equals("Main")) {
+            if("MultiList".equals(c.getName())) {
+                onMain_MultiListAction(c, event);
+                return;
+            }
+            if("MultiList1".equals(c.getName())) {
+                onMain_MultiList1Action(c, event);
+                return;
+            }
+            if("MultiList11".equals(c.getName())) {
+                onMain_MultiList11Action(c, event);
+                return;
+            }
+        }
+    }
+
+      protected void onMain_MultiListAction(Component c, ActionEvent event) {
+      }
+
+      protected void onMain_MultiList1Action(Component c, ActionEvent event) {
+      }
+
+      protected void onMain_MultiList11Action(Component c, ActionEvent event) {
+      }
+
+}
